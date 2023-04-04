a2b0808f385ef3903452ee2c3fd51e51ce943cc4
hiyoko <caio.demoraisfpinto@gmail.com>
2023-04-03 21:47:11 -0300

    add backend

---
+++ pizzas_maneiras_back/pizzas_maneiras_api/src/main/java/com/pizzas_maneiras/pizzas_maneiras_api/PizzasManeirasApiApplication.java
@@ -0,0 +1,13 @@
+package com.pizzas_maneiras.pizzas_maneiras_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzasManeirasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzasManeirasApiApplication.class, args);
+	}
+
+}
---
+++ pizzas_maneiras_back/pizzas_maneiras_api/src/test/java/com/pizzas_maneiras/pizzas_maneiras_api/PizzasManeirasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizzas_maneiras.pizzas_maneiras_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzasManeirasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
